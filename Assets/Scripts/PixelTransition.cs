ee8b10bc10bb37111f71b736a4d35677ef9d0f22
Lowliet <lowdata114@gmail.com>
2024-04-12 10:33:25 -0700

    Pixel Transition Temp

--- Assets/Scripts/PixelTransition.cs
+++ Assets/Scripts/PixelTransition.cs
@@ -1,32 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Pixelation.Example.Scripts;
 
-public class PixelTransition : MonoBehaviour
+public class PixelTransition : ImageEffectBase
 {
+    //BlockCount Amount
+    public float starting_amount = 512.0f;
     public float transition_amount = 128.0f;
-    public float transition_rate = 3.0f;
-    public float transition_duration = 2.0f;
+    public float transition_duration = 1.75f;
+    private float current_amount;
 
-    private float starting_amount;
-   // private float BlockCount; // Need a way to get access to pixelation to manipulate values
+    // private float BlockCount; // Need a way to get access to pixelation to manipulate values
     private float currentTime = 0.0f;
     private bool is_transitioning = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //insert way of getting starting amount from pixelation
-        //Store BlockCount variable
-    }
 
     // Update is called once per frame
     void Update()
     {
         currentTime += Time.deltaTime;
-        float lerp_value = Mathf.Clamp01(currentTime / transition_duration);
 
-
+        if (is_transitioning == true)
+        {
+            if (current_amount > transition_amount)
+            {
+                float lerp_value = Mathf.Clamp01(currentTime / transition_duration);
+                current_amount = Mathf.Lerp(current_amount, transition_amount, lerp_value);
+            }
+            else
+            {
+                is_transitioning = false;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,5 +40,17 @@ public class PixelTransition : MonoBehaviour
         {
             is_transitioning = true;
         }
+    }
+
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        float k = Camera.main.aspect;
+        Vector2 count = new Vector2(current_amount, current_amount / k);
+        Vector2 size = new Vector2(1.0f / count.x, 1.0f / count.y);
+        //
+        material.SetVector("BlockCount", count);
+        material.SetVector("BlockSize", size);
+        Graphics.Blit(source, destination, material);
     }
 }
